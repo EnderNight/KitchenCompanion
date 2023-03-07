6e335daf291a65f23085776839a7b6ede6851208
EnderNight <ndxendernight@gmail.com>
2023-03-07 22:22:05 +0100

    Added dialog to add food

--- src/desktop/main.py
+++ src/desktop/main.py
@@ -3,10 +3,28 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+
+class Handler:
+    def onFoodButtonClicked(self, stack):
+        stack.set_visible_child_name("foodsPage")
+
+    def onAddFoodClicked(self, args):
+        pass
+
+
+
 builder = Gtk.Builder()
 builder.add_from_file("main.glade")
+builder.connect_signals(Handler())
 
 window = builder.get_object("mainWindow")
+
+listsList = builder.get_object("listsList")
+listsList.add(Gtk.Label.new("Shopping list"))
+
+foodsList = builder.get_object("foodsList")
+foodsList.add(Gtk.Label.new("Foods list"))
+
 window.connect("destroy", Gtk.main_quit)
 window.show_all()
 
