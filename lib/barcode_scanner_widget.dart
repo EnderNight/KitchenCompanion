c4afda2eb355b16eb2eda07006db35244c9b509b
Ender <ndxendernight@gmail.com>
2024-10-02 00:19:58 +0200

    added barcode scanner widget

---
+++ lib/barcode_scanner_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
+import 'package:kitchen_companion/camera_feed_widget.dart';
+
+class BarcodeScannerWidget extends StatelessWidget {
+  final _barcodeScanner = BarcodeScanner();
+  final Function(List<String>) onBarcodes;
+
+  BarcodeScannerWidget({
+    super.key,
+    required this.onBarcodes,
+  });
+
+  void _scanBarcodes(InputImage image) async {
+    final barcodes = await _barcodeScanner.processImage(image);
+
+    onBarcodes(barcodes.map((b) => b.rawValue!).toList());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CameraFeedWidget(onImage: _scanBarcodes);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:kitchen_companion/barcode_scanner_widget.dart';
+
+class KitchenCompanionApp extends StatelessWidget {
+  const KitchenCompanionApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kitchen Companion',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? _scannedBarcode;
+
+  void _onBarcodes(List<String> codes) {
+    if (codes.isNotEmpty) {
+      setState(() {
+        _scannedBarcode = codes.first;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kitchen Companion'),
+      ),
+      body: Column(
+        children: [
+          BarcodeScannerWidget(onBarcodes: _onBarcodes),
+          Center(
+            child: Text(
+              _scannedBarcode == null
+                  ? 'No barcode detected'
+                  : _scannedBarcode!,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 void main() {
-  print('Hello World!');
+  runApp(const KitchenCompanionApp());
 }
